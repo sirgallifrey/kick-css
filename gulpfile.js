710c57704ad73b52abb2aefb4fc69904e66c6146
Adilson Schmitt Junior <adilsonschj@gmail.com>
2017-01-10 14:31:08 -0200

    working on project tolling

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,34 @@ const autoPrefixer = require('gulp-autoprefixer');
 const cssMin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const size = require('gulp-size');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload
 
 gulp.task('clean', function(cb){
     return gulp.src('./dist/*.css', { read: false })
         .pipe(rm());
 })
 
+const src = {
+    scss: './lib/**/*.scss',
+    html: './exemples/**/*.html'
+}
+
 const scssglobs = [
     './lib/**/*.scss',
     '!./lib/**/_*.scss'
 ]
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./exemples"
+    });
+
+    gulp.watch(src.scss, ['sass']);
+    gulp.watch(src.html).on('change', reload);
+});
 
 gulp.task('sass', ['clean'], function(){
     return gulp.src(scssglobs)
